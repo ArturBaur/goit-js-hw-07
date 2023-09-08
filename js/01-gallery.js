696157e7704c34899101c820ed78b9c3f6fbbc59
ArturBaur <artur.baur@gmail.com>
2023-09-09 00:19:30 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map(
+    (item) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryHTML);
+
+galleryContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "IMG") {
+    event.preventDefault();
+    const largeImageUrl = event.target.dataset.source;
+
+    const lightbox = basicLightbox.create(
+      `<img src="${largeImageUrl}" alt="Image description" />`
+    );
+    lightbox.show();
+  }
+});
+
+document.addEventListener("keydown", escapePress);
+function escapePress(eventKey) {
+  if (eventKey.code === "Escape") {
+    lightbox.close();
+    document.removeEventListener("keydown", escapePress);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map(
+    (item) => `
+  <li>
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryContainer.innerHTML = galleryHTML;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
